efd6d8c700880d4f59aa96c862ad847ee6be9ae2
JGreenOS <jsciteach@gmail.com>
2021-01-03 22:03:21 -0500

    Working date and beginning of form data

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- 
+var scheduledHours = [];
+
+//find and display current date and time//
+const day= moment().format("dddd, MMMM DD, YYYY hh:mm a");
+    console.log(day);
+$("#currentDay").text(day);
+
+//create blocks of time for scheduling//
+
+// for (var hour = 7; hour < 15 ; hour++) {
+//     scheduledHours.push(moment({hour}).format('h  a'));
+    
+//     $(".container").append(div class="row time-block")
+
+
+//scheduling column//
+
+
+    
+   
+    
+
+
