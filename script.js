2f9ed9f5f7b8346961f3284f672b8a14969ca0db
JGreenOS <jsciteach@gmail.com>
2021-01-10 13:21:31 -0500

    working on save button function

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-$( document ).ready(function() {})
+$(document).ready(function() {
+    
+
 
 //find and display current date and time//
 const day = moment().format("dddd, MMMM DD, YYYY hh:mm a");
 console.log(day);
 $("#currentDay").text(day);
 
-timeSlots = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-//localStorage.setItem('text-entry1', ""); //init the key with an empty value
-var time1 = document.getElementById('text-entry1');  //set variable to hold value
 
-button 1('click', function () {
-    localStorage.setItem('text-entry1', "");
 
 }
 )
 
-
+// $(document).ready(function() {
+//     // listen for save button clicks
+//     $(".saveBtn").on("click", function() {
+//       // get nearby values
+//       var value = $(this).siblings(".description").val();
+//       var time = $(this).parent().attr("id");
+  
+//       // save in localStorage
+//       localStorage.setItem(time, value);
+//     });
 
 // var saveBtn = $('.saveBtn');
 // saveBtn.on('click', function(){
 //     let eventId = $(this).attr('id');
 //     let eventText = $(this).parent().siblings().children('.event').val();
-//     localStorage.setItem(eventId, eventText);
-
+//     localStorage.setItem(eventId, eventText)
+})
