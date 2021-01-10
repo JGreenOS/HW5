7b313f96afe81ab31a601c6abcd788bcab41e0af
JGreenOS <jsciteach@gmail.com>
2021-01-10 13:34:26 -0500

    psedo-code for everything

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-$(document).ready(function() {
-    
-
+$(document).ready(function() { //make sure the page doesn't do funky stuff
 
 //find and display current date and time//
 const day = moment().format("dddd, MMMM DD, YYYY hh:mm a");
 console.log(day);
 $("#currentDay").text(day);
 
+//here I need to capture data from form when save button is clicked on only the line the button was clicked.
 
+
+//here I need to display back to the window the information from local storage in the same place as it was input (time and meeting).
+
+//here I need to set up what time it is right now
+var time = moment();
+console.log(time);
+
+//here I need to change the class of the row to the class of .present for the present time
+var currentTime = moment(hh);
+if currentTime === document.getElementById(time1)
+change the class to .present
+
+
+
+//here I need to change the class of the row to the class of .past when the time is after right now.
+
+if currentTime > document.getElementById(time1)
+change the class to .past
+
+
+//here I need to change the class of the row to the class of .future when the time is not yet.
+
+if currentTime < document.getElementById(time1)
+change class to .future
 
 
 }
 )
-
+//these rows are sample code to help figure out the syntax
 // $(document).ready(function() {
 //     // listen for save button clicks
 //     $(".saveBtn").on("click", function() {
@@ -29,4 +52,3 @@ $("#currentDay").text(day);
 //     let eventId = $(this).attr('id');
 //     let eventText = $(this).parent().siblings().children('.event').val();
 //     localStorage.setItem(eventId, eventText)
-})
